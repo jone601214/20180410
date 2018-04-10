c4a17c40b9c54281ecff723238639db38eeefc5c
jone601214 <32056035+jone601214@users.noreply.github.com>
2018-04-10 12:56:02 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+import javafx.scene.shape.Circle;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Circle circle1=new Circle();
+        Circle circle2=new Circle();
+        Circle circle3=new Circle();
+        System.out.println();
+        Circle circle4=new Circle();
+        Circle circle5=new Circle();
+        Circle circle6=new Circle();
+        System.out.println();
+    }
+}
---
+++ circle.java
@@ -0,0 +1,26 @@
+public class circle {
+    public static int count;
+    public circle(int radiusl1,int diameter1,int center1){
+        this.setRadiusl(radiusl1);
+        this.setDiameter(diameter1);
+        this.setCenter(center1);
+       }
+       public void setRadiusl(int radiusl1){
+        this.setRadiusl(radiusl1);
+       }
+       public int getCount(){
+           return count;
+       }
+       public void setDiameter(int diameter1){
+           this.setDiameter(diameter1);
+       }
+       public int getdiameter(){
+           return count;
+       }
+       public void setCenter(int center1){
+        this.setCenter(center1);
+    }
+       public int getcenter(){
+           return count;
+       }
+}
